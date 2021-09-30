7e0d868a779939e8420b756d440bb57d3cf358f7
Aidan <62295620+xIntensity9@users.noreply.github.com>
2021-09-30 15:53:08 -0400

    royalrobotics user
    royalrobo - pass

---
+++ TeamA 2021/src/robot-config.cpp
@@ -0,0 +1,30 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor frontLeft = motor(PORT1, ratio18_1, false);
+motor backLeft = motor(PORT2, ratio18_1, false);
+motor frontRight = motor(PORT3, ratio18_1, false);
+motor backRight = motor(PORT4, ratio18_1, false);
+motor clawLifter = motor(PORT5, ratio18_1, false);
+motor clawOpener = motor(PORT6, ratio18_1, false);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
